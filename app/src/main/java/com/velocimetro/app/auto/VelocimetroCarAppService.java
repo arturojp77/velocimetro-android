e60819578f176ab3ad510f4ad92bd5b88a43e5ed
arturojp77 <52300809+arturojp77@users.noreply.github.com>
2026-04-26 23:51:40 -0300

    Implement VelocimetroCarAppService class

--- app/src/main/java/com/velocimetro/app/auto/VelocimetroCarAppService.java
+++ app/src/main/java/com/velocimetro/app/auto/VelocimetroCarAppService.java
@@ -1,1 +1,21 @@
+package com.velocimetro.app.auto;
 
+import androidx.car.app.CarAppService;
+import androidx.car.app.Session;
+import androidx.car.app.validation.HostValidator;
+import androidx.annotation.NonNull;
+
+public class VelocimetroCarAppService extends CarAppService {
+
+    @NonNull
+    @Override
+    public Session onCreateSession() {
+        return new VelocimetroSession();
+    }
+
+    @NonNull
+    @Override
+    public HostValidator createHostValidator() {
+        return HostValidator.ALLOW_ALL_HOSTS_VALIDATOR;
+    }
+}
