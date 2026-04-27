48ebbbcb6f262830aebcdc21aeb398f4e35d533a
arturojp77 <52300809+arturojp77@users.noreply.github.com>
2026-04-26 23:54:00 -0300

    Implement VelocimetroSession with onCreateScreen method

--- app/src/main/java/com/velocimetro/app/auto/VelocimetroSession.java
+++ app/src/main/java/com/velocimetro/app/auto/VelocimetroSession.java
@@ -1,1 +1,15 @@
+package com.velocimetro.app.auto;
 
+import android.content.Intent;
+import androidx.car.app.Screen;
+import androidx.car.app.Session;
+import androidx.annotation.NonNull;
+
+public class VelocimetroSession extends Session {
+
+    @NonNull
+    @Override
+    public Screen onCreateScreen(@NonNull Intent intent) {
+        return new VelocimetroScreen(getCarContext());
+    }
+}
